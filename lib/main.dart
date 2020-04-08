7ebb504b7d7870892b1bd225a3af83a1a924e5b3
小林英里香 <kouyoumikan136@gmail.com>
2020-04-08 15:24:44 +0900

    #19 Card Class　カードの中に文字を表示する

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,31 @@ class _QuoteListState extends State<QuoteList> {
     Quote (author: "oscar wilde", text: "zxcvbnm")
   ];
 
+  Widget quoteTemplate(quote){ // カードを作成するクラス
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            quote.text,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.grey[600],
+            ),
+          ),
+          SizedBox(height: 6.0),
+          Text(
+            quote.text,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey[800],
+            )
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +53,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text("${quote.text} - ${quote.author}")).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
